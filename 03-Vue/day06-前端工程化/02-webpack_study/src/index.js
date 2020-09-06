818b883c083805534f4c69c0dad1ec058b9a7fc7
fengyehong123 <1355930128@qq.com>
2020-09-06 09:23:19 +0800

    update

--- 03-Vue/day06-前端工程化/02-webpack_study/src/index.js
+++ 03-Vue/day06-前端工程化/02-webpack_study/src/index.js
@@ -6,8 +6,6 @@ import './css/cssStyle.css'
 import './css/lessStyle.less'
 // 引入我们写的scss样式文件
 import './css/scssStyle.scss'
-// 引入我们写的Vue的单文件组件
-import App from './components/App.vue'
 
 // 使用jquery来实现各行变色
 $(function() {
@@ -21,3 +19,18 @@ class Person {
     static info = "infoMessage";
 }
 console.log(Person.info);
+
+// -----------------------------------------------
+// 导入vue构造函数
+import Vue from 'vue'
+// 引入我们写的Vue的单文件组件
+import App from './components/App.vue'
+
+
+const vm = new Vue({
+    // 3. 指定vm实例要控制的页面的区域
+    el: '#app',
+    // 4. 通过render函数,把指定的组件渲染到el区域中
+    
+    render: h => h(App)
+})
